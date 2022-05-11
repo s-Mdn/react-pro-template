672847d681cf59f8af32c8ff6a53b917ca2bbf68
yoyo <zhangyunbin@zhangyunbindeiMac.local>
2022-05-11 16:40:37 +0800

    完成规范

--- craco.config.js
+++ craco.config.js
@@ -1,39 +1,41 @@
-const path = require('path')
+const path = require("path")
 module.exports = {
-  style: {
-    modules: {
-      // localIdentName: '[file]__[name]__[hash:6]'  // class
-    },
-    postcssOptions: { // options 已经不适用
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
-    },
-  },
-  webpack: {
-    alias: {
-      '@src': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@utils': path.resolve(__dirname, './src/utils'),
-    }
-  },
-  babel: {
-    plugins: [
-      ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true}, 'antd'],
-      ["@babel/plugin-proposal-decorators", { "legacy": true }],
-    ]
-  },
-  plugins: [
-    {
-      plugin: require('craco-less'),
-      options: {
-        lessLoaderOptions: {
-          lessOptions: {
-            javascriptEnabled: true
-          }
-        }
-      }
-    }
-  ]
+	style: {
+		modules: {
+			// localIdentName: '[file]__[name]__[hash:6]'  // class
+		},
+		postcssOptions: {
+			// options 已经不适用
+			plugins: [require("tailwindcss"), require("autoprefixer")]
+		}
+	},
+	webpack: {
+		alias: {
+			"@src": path.resolve(__dirname, "./src"),
+			"@components": path.resolve(__dirname, "./src/components"),
+			"@utils": path.resolve(__dirname, "./src/utils")
+		}
+	},
+	babel: {
+		plugins: [
+			[
+				"import",
+				{ libraryName: "antd", libraryDirectory: "es", style: true },
+				"antd"
+			],
+			["@babel/plugin-proposal-decorators", { legacy: true }]
+		]
+	},
+	plugins: [
+		{
+			plugin: require("craco-less"),
+			options: {
+				lessLoaderOptions: {
+					lessOptions: {
+						javascriptEnabled: true
+					}
+				}
+			}
+		}
+	]
 }
