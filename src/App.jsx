3338fe15e37a5a61b23fa5586dc833385482f6f3
yoyo <zhangyunbin@zhangyunbindeiMac.local>
2022-05-20 16:00:17 +0800

    feat(src): 如果组件被按需加载了，那就无法拿到对应ref

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { Spin } from "antd"
 import classnames from "classnames"
 import { connect } from "react-redux"
@@ -7,11 +7,12 @@ import { bindActionCreators } from "redux"
 import NetworkError from "@components/NetworkError"
 import styles from "./global.module.less"
 
-const Home = React.lazy(() => import("./views/home"))
+// const Home = React.lazy(() => import("./views/home"))
+import Home from "./views/home"
 
 const App = props => {
-	const refs = React.useRef(null)
-	const [label] = React.useState("Click Me")
+	const node = useRef(null)
+	const [label] = useState("Click Me")
 	const spinner_root = `${styles["h-screen"]}  ${styles["flex"]} ${styles["justify-center"]} ${styles["items-center"]}`
 	const Spinner = () => (
 		<div className={classnames(spinner_root)}>
@@ -19,11 +20,20 @@ const App = props => {
 		</div>
 	)
 
+	// ref 转发高阶组件中使用
+	useEffect(() => {
+		console.log(node.current)
+	}, [node])
+
 	return (
 		<React.Fragment>
 			<NetworkError>
 				<React.Suspense fallback={<Spinner />}>
-					<Home label={label} ref={refs} />
+					{/* ref 转发 */}
+					{/* <Home label={label} /> */}
+					<Home label={label} ref={node} />
+					{/* ref 转发高阶组件中使用 */}
+					{/* <Home /> */}
 				</React.Suspense>
 			</NetworkError>
 		</React.Fragment>
--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -3,7 +3,7 @@ import "./index.less"
 
 // ref 转发
 // const Home = props => {
-// 	const refs = React.createRef()
+// 	const reff = React.createRef()
 // 	const FancyButton = React.forwardRef((prop, ref) => {
 // 		return (
 // 			<button ref={ref} className="FancyButton">
@@ -12,9 +12,9 @@ import "./index.less"
 // 		)
 // 	})
 // 	React.useEffect(() => {
-// 		console.log(refs)
+// 		console.log(reff)
 // 	})
-// 	return <FancyButton ref={refs}>Click me!</FancyButton>
+// 	return <FancyButton ref={reff}>Click me!</FancyButton>
 // }
 // export default Home
 
@@ -27,15 +27,22 @@ import "./index.less"
 // 		}
 
 // 		render() {
-// 			return <WrappedComponent {...this.props} />
+// 			const { forwardedRef, ...rest } = this.props
+// 			console.log("forwardedRef--->", forwardedRef)
+// 			return <WrappedComponent ref={forwardedRef} {...rest} />
 // 		}
 // 	}
 
-// 	return LogProps
+// 	return React.forwardRef((props, ref) => {
+// 		console.log("ref--->", ref)
+// 		return <LogProps {...props} forwardedRef={ref} />
+// 	})
 // }
 
 // class Home extends React.Component {
-// 	focus() {}
+// 	focus() {
+// 		console.log("聚焦了")
+// 	}
 
 // 	render() {
 // 		return <button className="FancyButton">{this.props.label}</button>
@@ -43,84 +50,90 @@ import "./index.less"
 // }
 // export default logProps(Home)
 
-// function logProps(Component) {
-// 	class LogProps extends React.Component {
-// 		componentDidUpdate(prevProps) {
-// 			console.log("old props:", prevProps)
-// 			console.log("new props:", this.props)
-// 		}
-
-// 		render() {
-// 			console.log(Component.name, Component.displayName)
-// 			const { forwardedRef, ...rest } = this.props
-
-// 			// 将自定义的 prop 属性 “forwardedRef” 定义为 ref
-// 			return <Component forwardedRef={forwardedRef} {...rest} />
-// 		}
-// 	}
-
-// 	function forwardRef(props, ref) {
-// 		return <LogProps {...props} forwardedRef={ref} />
-// 	}
-
-// 	// 在 DevTools 中为该组件提供一个更有用的显示名。
-// 	// 例如 “ForwardRef(logProps(MyComponent))”
-// 	// const name = Component.displayName || Component.name
-// 	// forwardRef.displayName = `logProps-(${name})`
-
-// 	// 注意 React.forwardRef 回调的第二个参数 “ref”。
-// 	// 我们可以将其作为常规 prop 属性传递给 LogProps，例如 “forwardedRef”
-// 	// 然后它就可以被挂载到被 LogProps 包裹的子组件上。
-// 	return React.forwardRef(forwardRef)
-// }
-// export default logProps
-
-function Son(props, ref) {
-	const inputRef = React.useRef(null)
-	const [inputValue, setInputValue] = React.useState("")
-
-	React.useImperativeHandle(
-		ref,
-		() => {
-			const handleRefs = {
-				onFocus() {
-					/* 声明方法用于聚焦input框 */
-					inputRef.current.focus()
-				},
-				onChangeValue(value) {
-					/* 声明方法用于改变input的值 */
-					setInputValue(value)
-				}
-			}
-			return handleRefs
-		},
-		[]
-	)
-
-	return (
-		<div>
-			<input placeholder="请输入内容" ref={inputRef} value={inputValue} />
-		</div>
-	)
+// 如何组件是被按需加载，那就会出现无法拿到ref,请注意该点
+function HOC(Component) {
+	class Wrap extends React.Component {
+		render() {
+			const { forwardedRef, ...otherprops } = this.props
+			return <Component forwardedRef={forwardedRef} {...otherprops} />
+		}
+	}
+	return React.forwardRef((props, ref) => {
+		return <Wrap forwardedRef={ref} {...props} />
+	})
 }
-
-const ForwarSon = React.forwardRef(Son)
 
 class Index extends React.Component {
-	cur = null
-	handerClick() {
-		const { onFocus, onChangeValue } = this.cur
-		onFocus() // 让子组件的输入框获取焦点
-		onChangeValue("let us learn React!") // 让子组件input
+	fucus() {
+		console.log("打印了")
 	}
 	render() {
-		return (
-			<div style={{ marginTop: "50px" }}>
-				<ForwarSon ref={cur => (this.cur = cur)} />
-				<button onClick={this.handerClick.bind(this)}>操控子组件</button>
-			</div>
-		)
+		return <div ref={this.props.forwardedRef}>hello,world</div>
 	}
 }
 
-export default Index
+const HocIndex = HOC(Index)
+
+export default HocIndex
+
+// export default Index
+// export default () => {
+// 	const node = React.useRef(null)
+// 	React.useEffect(() => {
+// 		console.log(node.current) /* Index 组件实例  */
+// 	}, [])
+// 	return (
+// 		<div>
+// 			<HocIndex ref={node} />
+// 		</div>
+// 	)
+// }
+// function Son(props, ref) {
+// 	const inputRef = React.useRef(null)
+// 	const [inputValue, setInputValue] = React.useState("")
+
+// 	React.useImperativeHandle(
+// 		ref,
+// 		() => {
+// 			const handleRefs = {
+// 				onFocus() {
+// 					/* 声明方法用于聚焦input框 */
+// 					inputRef.current.focus()
+// 				},
+// 				onChangeValue(value) {
+// 					/* 声明方法用于改变input的值 */
+// 					setInputValue(value)
+// 				}
+// 			}
+// 			return handleRefs
+// 		},
+// 		[]
+// 	)
+
+// 	return (
+// 		<div>
+// 			<input placeholder="请输入内容" ref={inputRef} value={inputValue} />
+// 		</div>
+// 	)
+// }
+
+// const ForwarSon = React.forwardRef(Son)
+
+// class Index extends React.Component {
+// 	cur = null
+// 	handerClick() {
+// 		const { onFocus, onChangeValue } = this.cur
+// 		onFocus() // 让子组件的输入框获取焦点
+// 		onChangeValue("let us learn React!") // 让子组件input
+// 	}
+// 	render() {
+// 		return (
+// 			<div style={{ marginTop: "50px" }}>
+// 				<ForwarSon ref={cur => (this.cur = cur)} />
+// 				<button onClick={this.handerClick.bind(this)}>操控子组件</button>
+// 			</div>
+// 		)
+// 	}
+// }
+
+// export default Index
