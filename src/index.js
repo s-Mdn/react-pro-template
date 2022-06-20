6947d6faa3bace2205e42b675e1a1da89b476a4d
s-Mdn <1135534394@qq.com>
2022-06-20 23:13:01 +0800

    fix(src/index): 增加git cz 测试'

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import { Provider } from "react-redux"
 import store from "./store"
 import App from "./App"
 import("./utils/network")
-
+console.log(App)
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
 root.render(
 	<Provider store={store}>
-		<App />
+		<App>1</App>
 	</Provider>
 )
